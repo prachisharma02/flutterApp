90093b7d64242135273156b96136019deb4bc006
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2023-10-27 10:10:57 +0530

    added textarea

--- lib/textArea.dart
+++ lib/textArea.dart
@@ -20,10 +20,10 @@ class Mycustomform extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Padding(
+        Padding(
           padding: EdgeInsets.symmetric(horizontal: 0, vertical: 16),
           child: TextField(
             maxLength: 12,
@@ -34,6 +34,17 @@ class Mycustomform extends StatelessWidget {
               hintText: "enter text with 12 character",
             ),
           ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 0, vertical: 16),
+          child: TextField(
+              maxLength: 12,
+              decoration: InputDecoration(
+                prefixText: "mca 2",
+                suffixText: "best wishes",
+                border: OutlineInputBorder(),
+                hintText: "enter text with 12 character",
+              )),
         )
       ],
     );
